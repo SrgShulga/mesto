88d3a54253228f641cd11080c1eb7326785a0573
Sergei Shulga <k1ft4999@gmail.com>
2023-04-19 18:19:01 +0300

    feat: Сделать отображение карточек через JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const popup = document.querySelector('.popup');
 const openPopupBtn = document.querySelector('.profile__edit-btn');
 const closePopupBtn = document.querySelector('.popup__close-btn');
@@ -7,17 +34,20 @@ const inputName = document.querySelector('.popup__input_data_name');
 const inputDescription = document.querySelector('.popup__input_data_description');
 const editProfileForm = document.querySelector('.popup__form')
 
-function openPopup () {
+const cardTemplate = document.querySelector('#elements-template');
+const cardContainer = document.querySelector('.elements');
+
+const openPopup = () => {
   popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 }
 
-function closePopup () {
+const closePopup = () => {
   popup.classList.remove('popup_opened');
 }
 
-function handleFormSubmit(evt) {
+const handleFormSubmit = (evt) => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
@@ -27,3 +57,36 @@ function handleFormSubmit(evt) {
 openPopupBtn.addEventListener('click',openPopup);
 closePopupBtn.addEventListener('click', closePopup);
 editProfileForm.addEventListener('submit', handleFormSubmit);
+
+const createCardElement = (cardData) => {
+  const cardElement = cardTemplate.content.querySelector('.elements__card').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.elements__card-image');
+  const cardTitle = cardElement.querySelector('.elements__title');
+  const cardDeleteButton = cardElement.querySelector('.elements__delete-btn');
+  const cardLikeButton = cardElement.querySelector('.elements__like-btn');
+
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+
+  const handleDelete = () => {
+    cardElement.remove();
+  };
+
+  const handleLike  = (evt) => {
+    cardLikeButton.classList.toggle('elements__like-btn_active');
+  };
+
+  cardLikeButton.addEventListener('click', handleLike);
+  cardDeleteButton.addEventListener('click', handleDelete);
+
+  return cardElement;
+};
+
+initialCards.forEach((card) => {
+  const element = createCardElement(card);
+
+  cardContainer.prepend(element);
+});
+
